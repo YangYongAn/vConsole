80b53f01d500421e3b3caa2ce4d5f20eea5b4c46
YangYongAn <i@yangyongan.com>
2019-08-27 13:53:05 +0800

    apicloud相关打日志方案处理

---
+++ src/log/apicloud.js
@@ -0,0 +1,46 @@
+(function (window) {
+  function _log(args) {
+    var log = {
+      '■': [],
+      W: api.winName,
+      F: api.frameName,
+      U: location.pathname.split('/html/')[1]
+    };
+
+    for (var i = 0; i < args.length; i++) {
+      if (typeof args[i] == 'object') {
+        log['■'][i] = JSON.stringify(args[i]);
+      } else {
+        log['■'][i] = args[i];
+      }
+    }
+
+    return JSON.stringify(log)
+  }
+
+  /**
+   * 普通日志
+   * @param 任意类型 多个使用,分割
+   */
+  function log() {
+    console.log(_log(arguments));
+  }
+
+  /**
+   * 错误日志
+   */
+  function error() {
+    console.error(_log(arguments));
+  }
+
+  /**
+   * 警告日志
+   */
+  function warn() {
+    console.warn(_log(arguments));
+  }
+
+  window.L = window.log = log;
+  window.E = window.error = error;
+  window.W = window.warn = warn;
+})(window)
